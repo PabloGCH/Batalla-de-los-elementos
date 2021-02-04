cb3d1431c83d83ae785ba7c51f6fc01e8c320774
PabloGCH <pablochoconi@gmail.com>
2021-02-04 15:05:13 -0300

    intente pasar los archivos a src

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
-#include <iostream>
-#include <string>
+#include "ABB.h"
+#include "personaje_fuego.h"
+#include "nodo.h"
 
-#include "casillero.h"
-#include "character.h"
-#include "elements.h"
-#include "tablero.h"
+using namespace std;
 
 int main(){
-	Tablero prueba;
-	prueba.printBoard();
-}
+    Dato Jugador1 = new PersonajeFuego("Lucio", 1, 76);
+    Dato Jugador2 = new PersonajeFuego("Maria", 1, 76);
+    Dato Jugador3 = new PersonajeFuego("Gimeno", 1, 76);
+    Dato Jugador4 = new PersonajeFuego("Albert", 1, 76);
+    Dato Jugador5 = new PersonajeFuego("Carlos", 0, 21);
+   // Dato Jugador6 = new PersonajeFuego("Karlos", 0, 21);
 
+
+    ABB diccionario;
+    diccionario.insertarHoja(Jugador1);
+    diccionario.insertarHoja(Jugador2);
+    diccionario.insertarHoja(Jugador3);
+    diccionario.insertarHoja(Jugador4);
+    diccionario.insertarHoja(Jugador5);
+    //diccionario.insertarHoja(Jugador6);
+    diccionario.enOrden();
+   // Nodo* encontrado = diccionario.buscarPersonaje(Jugador4->obtenerNombre());
+   // Dato pers = encontrado->obtenerDato();
+   // pers->mostrarAtributos();
+   // Dato menor = diccionario.encontrarMenor();
+    diccionario.borrarNodo("Gimeno");
+    diccionario.enOrden();
+
+    return 0;
+}

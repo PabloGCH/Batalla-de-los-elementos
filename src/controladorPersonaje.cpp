7f4aad931b1b319d04b37260d0ebb4b0d9ca00c0
PabloGCH <pablochoconi@gmail.com>
2021-02-09 17:45:32 -0300

    Agregada clase ControladorPersonaje, con metodos para encontrar caminos etc.

--- src/controladorPersonaje.cpp
+++ src/controladorPersonaje.cpp
@@ -2,6 +2,7 @@
 
 ControladorPersonaje::ControladorPersonaje() {
 	personaje = 0;
+    for(int i = 0; i<2; i++){ubicacion[i] = 0;};
     ptrCasillero = 0;
 }
 
@@ -19,7 +20,6 @@ void ControladorPersonaje::etapa1() {
                 salir = true;
                 break;
             case MOVERSE:
-                moverse();
                 salir = true;
                 break;
             case PASAR:
@@ -69,3 +69,95 @@ void ControladorPersonaje::imprimirOpciones2() {
 	
 }
 
+
+void ControladorPersonaje::pathfind(int *actualCoord, Casillero* actualPtr){
+    Casillero* current = actualPtr;
+    bool visto[64];
+    int currentID = coordToId(actualCoord);
+    int currentCoord[2] = {0,0};
+    int cantVistos = 0;
+    for(int i = 0; i < 64; i++){
+        costo[i] = 1000;
+        visto[i] = false;
+    }
+    //inicializo
+    evaluarNodos(currentID, visto, current);
+    anterior[0] = 0;
+    costo[currentID] = 0;
+    visto[currentID] = true;
+    cantVistos++;
+    while(cantVistos < 64){
+        currentID = buscarMinimo(costo, visto);
+        idToCoord(currentID, currentCoord);
+        current = tablero->returnItem(currentCoord);
+        evaluarNodos(currentID, visto, current);
+        cantVistos++;
+    }
+}
+
+int ControladorPersonaje::coordToId(int *coord){
+    int ID = (((coord[0] - 1) * 8) + coord[1]) -1;
+    return ID;
+}
+
+void ControladorPersonaje::idToCoord(int ID, int* coord){
+    coord[0] = (ID / 8) + 1;
+    coord[1] = (ID % 8);
+}
+
+int ControladorPersonaje::buscarMinimo(int* vector, bool* visto){
+    int minimo = 1000;
+    int minPos = 0;
+    for(int i = 0; i < 64 ; i++){
+        if((vector[i] < minimo) && (visto[i] == false)){
+            minimo = vector[i];
+            minPos = i;
+        }
+    }
+    return minPos;
+}
+
+void ControladorPersonaje::evaluarNodos(int currentID, bool* visto, Casillero* current){
+    int auxID = 0;
+    int auxCosto = 0;
+    for(int i = 1; i <= 4; i++){
+        switch(i){
+            case ARRIBA:
+                //consigo valor del nodo de arriba
+                auxID = currentID -8;
+                auxCosto = evaluarDir(current->getUp());
+                break;
+            case DERECHA:
+                //consigo valor del nodo a la derecha
+                auxID = currentID +1;
+                auxCosto = evaluarDir(current->getRight());
+                break;
+            case ABAJO:
+                // consigo valor del nodo de abajo
+                auxID = currentID +8;
+                auxCosto = evaluarDir(current->getDown());
+                break;
+            case IZQUIERDA:
+                //consigo valor del nodo a la izquierda
+                auxID = currentID -1;
+                auxCosto = evaluarDir(current->getLeft());
+                break;
+        }
+        if((auxCosto < costo[auxID]) && (visto[auxID] == false)){
+            costo[auxID] = auxCosto;
+            anterior[auxID] = currentID;
+        }
+    }
+}
+
+
+
+
+
+//============================================================
+//============================================================
+//============================================================
+//============================================================
+void ControladorPersonaje::moverse(){
+
+}
--- src/controladorPersonaje.h
+++ src/controladorPersonaje.h
@@ -1,6 +1,7 @@
 #ifndef CONTROLADORPERSONAJE_H
 #define CONTROLADORPERSONAJE_H
 #include "tablero.h"
+#include "personaje_agua.h"
 
 enum opciones1{
     //etapa 1
@@ -13,12 +14,22 @@ enum opciones1{
     PASAR = 3,
 };
 
+enum direcciones{
+    ARRIBA = 1,
+    DERECHA = 2,
+    ABAJO = 3,
+    IZQUIERDA = 4,
+};
+
 class ControladorPersonaje
 {
     protected:
         Personaje* personaje;
         Casillero* ptrCasillero;
-        int ubicacion[2] = {1, 2};
+        Tablero* tablero;
+        int ubicacion[2];
+        int costo[64];
+        int anterior[64];
     public:
         ControladorPersonaje();
         // Da a elegir Alimentarse, Moverse, Pasar Opcion
@@ -32,8 +43,25 @@ class ControladorPersonaje
         void imprimirOpciones2();
 
 
-        virtual void moverse() = 0;
         virtual void atacar() = 0;
+        // evalue coste de caminos
+        virtual int evaluarDir(Casillero* dir) = 0;
+        // busca camino minimo
+        void pathfind(int *actualCoord, Casillero* actualPtr);
+        // extrae camino del vector anterior
+        void getPath(int destino);
+        // evaluo Distancia, Nodos circundantes
+        void evaluarNodos(int currentID, bool* visto, Casillero* current);
+        // convierte coordenadas en numero de elemento
+        int coordToId(int *coord);
+        // convierte ID en coordenadas
+        void idToCoord(int ID, int* coord);
+        // convierte numero en coordenadas
+        int buscarMinimo(int* vector, bool* visto);
+        // Si el personaje tiene energia lo mueve en esa direccion
+        void moverse();
+        
+
 };
 
 #endif

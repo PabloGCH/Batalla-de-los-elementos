02a54a9443c6539f0c00c191eebd552ba09faaa8
RonanFilloy <77989054+RonanFilloy@users.noreply.github.com>
2021-02-11 14:30:16 -0300

    Update jugador.cpp

--- src/jugador.cpp
+++ src/jugador.cpp
@@ -1,15 +1,21 @@
 #include "jugador.h"
 
-Jugador::Jugador(ControladorPersonaje* 1, ControladorPersonaje* 2, ControladorPersonaje* 3) {
-	ctrlpersonaje1 = 1;
-	ctrlpersonaje2 = 2;
-	ctrlpersonaje3 = 3;
+Jugador::Jugador() {
+	ctrlpersonaje1 = 0;
+	ctrlpersonaje2 = 0;
+	ctrlpersonaje3 = 0;
 	oponente = 0;
 	pos_enemigos = 0;
 }
 
 void Jugador::asignar_rival(Jugador* rival) {
 	oponente = rival;
+}
+
+void Jugador::asignar_controlador(ControladorPersonaje* 1, ControladorPersonaje* 2, ControladorPersonaje* 3) {
+	ctrlpersonaje1 = 1;
+	ctrlpersonaje2 = 2;
+	ctrlpersonaje3 = 3;
 }
 
 int Jugador::ubicacion_personajes() {
@@ -22,4 +28,10 @@ int Jugador::ubicacion_personajes() {
 
 void Jugador::obtener_pos_enemigos() {
 	pos_enemigos = oponente->ubicacion_personajes();
+}
+
+Jugador::~Jugador() {
+	delete (ctrlpersonaje1);
+	delete (ctrlpersonaje2);
+	delete (ctrlpersonaje3);
 }

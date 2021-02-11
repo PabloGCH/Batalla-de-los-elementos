afcabbb7683b57f6919c4f55eba0c9246b5e7882
RonanFilloy <77989054+RonanFilloy@users.noreply.github.com>
2021-02-11 14:31:02 -0300

    Update jugador.h

--- src/jugador.h
+++ src/jugador.h
@@ -1,19 +1,23 @@
 #ifndef JUGADOR_H_
 #define JUGADOR_H_
 
-#include "ControladorPersonaje.h"
+#include "controladorersonaje.h"
 
 class Jugador {
-public:
+protected:
 	ControladorPersonaje* ctrlpersonaje1;
 	ControladorPersonaje* ctrlpersonaje2;
 	ControladorPersonaje* ctrlpersonaje3;
 	int* pos_enemigos[3];
 	Jugador* oponente;
-	Jugador(ControladorPersonaje* 1, ControladorPersonaje* 2, ControladorPersonaje* 3);
+
+public:
+	Jugador();
+	void asignar_controlador(ControladorPersonaje* 1, ControladorPersonaje* 2, ControladorPersonaje* 3);
 	void asignar_rival(Jugador* rival);
 	void obtener_pos_enemigos();
 	int ubicacion_personajes();
+	~Jugador();
 };
 
 

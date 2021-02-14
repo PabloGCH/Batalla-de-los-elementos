5f5366522125f3a5343c5c07205615b5d538f714
Luciano <lsalerno@fi.uba.ar>
2021-02-14 19:41:36 -0300

    Agregados Metodos: -agreagarPersonaje ( para agreagar por consola por el jugador) -procesarArchivo ( es el que agrega lso personajes del archivo al diccionario ). Al Constructor solo debe pasarse la ruta del archivo.  Y varios metodos mas para que estos dos metodos haga todo el trabajo. (todos privados)

--- src/juego.cpp
+++ src/juego.cpp
@@ -1,7 +1,8 @@
 #include "juego.h"
 
-Juego::Juego(){
+Juego::Juego(ifstream archivo){
     //Lleno diccionario con archivo
+    procesarArchivo(archivo);
 }
 
 void Juego::iniciar(){
@@ -11,14 +12,12 @@ void Juego::iniciar(){
     opcionesPersonaje();
 }
 
-
 int Juego::recibirOpcion(){
     int opcion;
     cout << "\nIngrese una opcion: ";
     cin >> opcion;
     return opcion;
 }
-
 
 void Juego::imprimirOpcionesPersonajes(){
     cout << "----------BATALLA DE LOS ELEMENTOS----------" << endl;
@@ -74,6 +73,97 @@ void Juego::opcionesPersonaje(){
     }
 }
 
+void Juego::agregarPersonaje() {
+    string elementoAgregar, nombreAgregar;
+    Dato personajeAgregar;
+
+    cout << "\tAGREGAR PERSONAJE." << endl;
+    cin.ignore();
+    registrarElemento(elementoAgregar);
+
+    cout << "Ingrese el nombre del nuevo personaje: ";
+    leerCadena(nombreAgregar);
+    while(nombreAgregar == (diccionario.buscarPersonaje(nombreAgregar))->obtenerClave()){
+        cout << "El personaje que quiere agregar ya existe. Ingrese otro nombre para un nuevo personaje." << endl;
+        leerCadena(nombreAgregar);
+    }
+    personajeAgregar = crearPersonaje(elementoAgregar, nombreAgregar);
+    diccionario.insertarHoja(personajeAgregar);
+}
+
+void Juego::leerCadena(string &cadena){
+    getline(cin, cadena);
+    nombreMayuscula(cadena);
+}
+
+void Juego::nombreMayuscula(string &nombre){
+    for(int i = 0; i < (int)nombre.length(); i++){
+        if(i == 0)
+            nombre[i] = toupper(nombre[i]);
+        else if(nombre[i-1] != ' ')
+            nombre[i] = tolower(nombre[i]);
+        if(nombre[i] == ' ')
+            nombre[i+1] = toupper(nombre[i+1]);
+    }
+}
+
+Dato Juego::crearPersonaje(string elemento, string nombre){
+    Dato nuevo;
+    int escudo = rand() % 3;
+    int vida = rand() % 91 + 10;
+    if(elemento == "AGUA")
+        nuevo = new PersonajeAgua(nombre, escudo, vida);
+    else if(elemento == "AIRE")
+        nuevo = new PersonajeAire(nombre, escudo, vida);
+    else if(elemento == "FUEGO")
+        nuevo = new PersonajeFuego(nombre, escudo, vida);
+    else
+        nuevo = new PersonajeTierra(nombre, escudo, vida);
+    return nuevo;
+}
+
+Dato Juego::crearPersonaje(string elemento, string nombre, int escudo, int vida){
+    Dato nuevo;
+    if(elemento == "AGUA")
+        nuevo = new PersonajeAgua(nombre, escudo, vida);
+    else if(elemento == "AIRE")
+        nuevo = new PersonajeAire(nombre, escudo, vida);
+    else if(elemento == "FUEGO")
+        nuevo = new PersonajeFuego(nombre, escudo, vida);
+    else
+        nuevo = new PersonajeTierra(nombre, escudo, vida);
+    return nuevo;
+}
+
+void Juego::registrarElemento(string &elementoAgregar){
+    cout << "Ingrese el elemento del nuevo personaje ( AGUA, AIRE, TIERRA, FUEGO ): ";
+    leerCadena(elementoAgregar);
+    while(elementoAgregar != "Agua" && elementoAgregar != "Aire" && elementoAgregar != "Tierra" && elementoAgregar != "Fuego") {
+        cout << "\tERROR. Ingrese un elemento válido ( AGUA, AIRE, TIERRA, FUEGO ): ";
+        leerCadena(elementoAgregar);
+    }
+}
+
+void Juego::procesarArchivo(ifstream &archivo){
+    if (archivo.is_open()){
+        while(!archivo.eof()){
+            string elemento, nombre, escudo, vida;
+            int escudoEntero, vidaEntero;
+            procesarDatosPersonaje(archivo, elemento, nombre, escudo, vida, escudoEntero, vidaEntero);
+            Dato nuevo = crearPersonaje(elemento, nombre, escudoEntero, vidaEntero);
+            diccionario.insertarHoja(nuevo);
+        }
+    }
+}
+
+void Juego::procesarDatosPersonaje(ifstream &archivo, string &elemento, string &nombre, string &escudo, string &vida, int &escudoEntero, int &vidaEntero){
+    getline(archivo, elemento, ',');
+    getline(archivo, nombre, ',');
+    getline(archivo, escudo, ',');
+    getline(archivo, vida, '\n');
+    escudoEntero = stoi(escudo);
+    vidaEntero = stoi(vida);
+}
 
 void Juego::comenzarJuego(){
     bool salir = false;
--- src/juego.h
+++ src/juego.h
@@ -9,7 +9,9 @@ using namespace std;
 class Juego{
     public:
         void iniciar();
-        Juego();
+        // POS: agrega un nuevo personaje al diccionario.
+        void agregarPersonaje();
+        Juego(ifstream archivo);
     private:
         void opcionesPersonaje();
         void comenzarJuego();
@@ -18,6 +20,30 @@ class Juego{
         void imprimirOpcionesComenzar();
         int recibirOpcion();
         void pedirOpcionJugador(int i);
+
+        //POS: Registra un elemento valido para la creacion de un personaje ( AIRE, AGUA, FUEGO, TIERRA )
+        void registrarElemento(string &elementoAgregar);
+
+        // POS: Devuelve un puntero al nuevo personaje.
+        Dato  crearPersonaje(string elemento, string nombre);
+
+        // Metodo sobrecargado para crear personajes desde archivo con valores de escudo y vida ya dterminados.
+        // POS: Devuelve un puntero al nuevo personaje.
+        Dato crearPersonaje(string elemento, string nombre, int escudo, int vida);
+
+        //POS: lee una cadena ingresada por teclado y la deja con la primer letra de cada palabra en mayusculas.
+        void leerCadena(string &cadena);
+
+        //POS: Deja la 1° letra de cada palabra en mayuscula.
+        void nombreMayuscula(string &nombre);
+
+        // PRE: El archivo debe pasarse abierto.
+        // POS carga todos los personajes en el archivo en el diccionario.
+        void procesarArchivo(ifstream &archivo);
+
+        // PRE: el archivo se debe pasar abierto.
+        // POS: Almacena 'elemento', 'nombre', 'escudo', y 'vida' en variables y convierte estas ultimas 2 a INT.
+        void procesarDatosPersonaje(ifstream &archivo, string &elemento, string &nombre, string &escudo, string &vida, int &escudoEntero, int &vidaEntero);
         ABB diccionario;
         Jugador jugadores[2];
         Tablero tablero;
